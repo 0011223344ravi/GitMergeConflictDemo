bed75d2cc9c9b622157b4a4d352b4d6a11033f7a
Ravi Kumar Gupta <babaravibbb@gmail.com>
2023-08-22 23:28:27 +0530

    first commit

---
+++ src/main/java/com/example/gitmergeconflictdemo/GitMergeConflictDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitmergeconflictdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergeConflictDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMergeConflictDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitmergeconflictdemo/GitMergeConflictDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitmergeconflictdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMergeConflictDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
